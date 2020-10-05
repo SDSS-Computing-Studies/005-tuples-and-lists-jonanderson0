b5e13aabb0d27c88fd9ac339933186f3a215fe8a
jonanderson0 <71406833+jonanderson0@users.noreply.github.com>
2020-10-05 12:39:15 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -25,17 +25,3 @@ The animal at that index is Dog
 
 
 tup =("Cat","Fish","Dog","Bear","Turtle")
-def Sort(tup): 
-      
-    n = len(tup) 
-      
-    for i in range(n): 
-        for j in range(n-i-1): 
-              
-            if tup[j][0] > tup[j + 1][0]: 
-                tup[j], tup[j + 1] = tup[j + 1], tup[j] 
-                  
-    return tup
-     
-          
-print(Sort(tup))
