7294518c9658f4a26e4a08dcab0050004748a464
jonanderson0 <71406833+jonanderson0@users.noreply.github.com>
2020-10-06 11:23:21 -0700

    Update task3.py

--- task3.py
+++ task3.py
@@ -13,4 +13,7 @@ example:
 The smallest number is 3 and the largest number is 9
 """
 
-myList = [ 3,6,5,4,6,7,8,6,5,9,4,5 ]
+myList.sort()
+low = myList[0]
+high = myList[-1]
+print("The smallest number is  "+str(low)+ " and the largest number is " +str(high))
